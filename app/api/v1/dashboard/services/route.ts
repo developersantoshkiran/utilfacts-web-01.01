82969903ccf77caa34030a5e708dc6baa0977deb
developersantoshkiran <developer.santoshkiran@gmail.com>
2026-08-29 08:07:06 +0530

    reslove typescript errors

--- app/api/v1/dashboard/services/route.ts
+++ app/api/v1/dashboard/services/route.ts
@@ -1,4 +1,3 @@
-
 "use server";
 
 import { NextRequest, NextResponse } from "next/server";
@@ -6,11 +5,11 @@ import { getSession } from "@/app/_components/auth";
 import { db } from "@/app/_components/services/dbconnection";
 import { sql } from "drizzle-orm";
 
-export async function GET(req: NextRequest, { params }: { params: Promise<{ entityid: string }> }) {
+export async function GET(req: NextRequest) {
     try {
         let session = await getSession();
         const searchParams = req.nextUrl.searchParams;
-        let includeInactive = searchParams.get('includeInactive')
+        let includeInactive = searchParams.get('includeInactive');
         let entityId = searchParams.get('entityId');
         let projectID = '';
         const selectedProject = session?.selectedAdminProject;
@@ -20,19 +19,19 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ enti
         }
         else if (session?.selectedUserEntity) {
             entityId = session.selectedUserEntity.id;
-            projectID = session.selectedUserEntity.projectid
+            projectID = session.selectedUserEntity.projectid;
         }
         else {
-            return NextResponse.json({ message: "You dont have any projects assigned" })
+            return NextResponse.json({ message: "You dont have any projects assigned" });
         }
 
         const finalSql = sql.empty();
 
         finalSql.append(sql`select service, json_arrayagg (jsonb_build_object('id',s.id, 'name', sub_service, 'unit',unit, 'color', color, 'paymentType', "paymentType", 'active', active, 'bill_generation_date', s.bill_generation_date, 'due_date', s.due_date`);
         if (entityId)
-            finalSql.append(sql`,'entity_service_id', es.id`)
+            finalSql.append(sql`,'entity_service_id', es.id`);
 
-        finalSql.append(sql` )) AS sub_services FROM services s left join projects p on s.project_id = p.id`)
+        finalSql.append(sql` )) AS sub_services FROM services s left join projects p on s.project_id = p.id`);
         if (entityId) {
             finalSql.append(sql` left join entity_services es on es.service_id = s.id`);
         }
@@ -48,11 +47,11 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ enti
         }
         finalSql.append(sql` GROUP BY s.service`);
 
-        const { rows } = await db.execute(finalSql)
+        const { rows } = await db.execute(finalSql);
 
         return NextResponse.json(rows);
 
     } catch (e: any) {
-        return NextResponse.json({ message: e?.message }, { status: 500 })
+        return NextResponse.json({ message: e?.message }, { status: 500 });
     }
-};
+}
